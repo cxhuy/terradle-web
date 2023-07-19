61290053ad5d90e33aa1d8e52fe0c4e9abd887d4
cxhuy <alvinkang1@gmail.com>
2023-07-19 21:10:54 +0900

    feat: add main logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
     import "../app.css";
 </script>
 
-<div class="min-h-screen w-full flex bg-black/50 backdrop-blur-sm">
+<div class="min-h-screen w-full flex bg-black/30 backdrop-blur-sm">
+    <div class="mt-8 w-full h-32 p-3">
+        <img class="mx-auto h-full drop-shadow-2xl" src="/src/lib/logo.png" alt="logo">
+    </div>
     <slot />
 </div>
