344a8ff9f5b4712aa5aca96b34c2209f39ae1e0b
cxhuy <alvinkang1@gmail.com>
2023-07-21 00:25:45 +0900

    feat: add top ui layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,10 @@
 </script>
 
 <div class="min-h-screen w-full flex bg-black/30 backdrop-blur-sm">
-    <div class="mt-8 w-full h-32 p-3">
-        <img class="mx-auto h-full drop-shadow-2xl" src="/src/lib/logo.png" alt="logo">
+    <div class="w-full">
+        <div class="mt-8 w-full h-24 px-3">
+            <img class="mx-auto w-80 drop-shadow-2xl" src="/src/lib/logo2.png" alt="logo">
+        </div>
+        <slot />
     </div>
-    <slot />
 </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<main class="-translate-y-4">
+    <div class="mx-auto translate-y-1/2 w-64 h-14 bg-[#495EAB] border-[1.5px] border-black rounded-3xl flex items-center">
+        <p style="-webkit-text-stroke: 0.5px black;" class="mx-auto text-2xl">Guess today's weapon!</p>
+    </div>
+    <div class="mx-auto w-80 h-fit bg-[#1C2443] border-[1.5px] border-black rounded-xl flex flex-col pt-8 px-2 pb-2">
+        <div class="w-full flex gap-2 mb-2">
+            <div class="w-full h-20 bg-[#2C3A74] border-[1.5px] border-black rounded-lg"></div>
+            <div class="w-full h-20 bg-[#2C3A74] border-[1.5px] border-black rounded-lg"></div>
+            <div class="w-full h-20 bg-[#2C3A74] border-[1.5px] border-black rounded-lg"></div>
+        </div>
+        <div class="w-full">
+            <input class="w-full h-9 bg-[#2C3A74] border-[1.5px] border-[#4157A4] rounded-lg" type="text">
+        </div>
+    </div>
+</main>
