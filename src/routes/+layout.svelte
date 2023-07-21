a0ca91ae6cbe50b3b2ff51a07a3f89e7c22250b0
cxhuy <alvinkang1@gmail.com>
2023-07-21 21:03:23 +0900

    fix: fix logo src

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 <div class="min-h-screen w-full flex bg-black/30 backdrop-blur-sm">
     <div class="w-full">
         <div class="mt-8 w-full h-24 px-3">
-            <img class="mx-auto w-80 drop-shadow-2xl" src="/src/lib/logo2.png" alt="logo">
+            <img class="mx-auto w-80 drop-shadow-2xl" src="/src/lib/logo.png" alt="logo">
         </div>
         <slot />
     </div>
