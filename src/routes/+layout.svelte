5b70532a555a0fa03f759b0a31d266f43d0af942
cxhuy <alvinkang1@gmail.com>
2023-08-17 00:27:00 +0900

    rename: move images from lib to static folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 <div class="h-screen w-full flex bg-black/30 backdrop-blur-sm overflow-auto">
     <div class="w-full">
         <div class="mt-8 w-full h-24 px-3">
-            <img class="mx-auto w-80 drop-shadow-2xl" src="/src/lib/logo.png" alt="logo">
+            <img class="mx-auto w-80 drop-shadow-2xl" src="/logo.png" alt="logo">
         </div>
         <slot />
     </div>
