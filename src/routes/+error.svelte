936b8abbb93fb66508d421ecec813511ee5febfe
cxhuy <alvinkang1@gmail.com>
2023-08-24 20:13:46 +0900

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="w-full text-center">
+    <h1 class="mt-20 text-5xl">{$page.status} {$page.error.message}</h1>
+    {#if $page.status == 500}
+        <p class="text-2xl">Try refreshing the page</p>
+    {:else if $page.status == 404}
+        <a href="/" class="text-2xl hover:underline">Go to home</a>
+    {/if}
+</div>
