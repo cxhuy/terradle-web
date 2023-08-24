bc59fd0a9ae4a8ca8c12c2e89a54a82cac5ad99f
cxhuy <alvinkang1@gmail.com>
2023-08-24 19:52:21 +0900

    feat: add google analytics

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+
+    $: {
+        if (typeof gtag !== "undefined") {
+            gtag("config", "G-Y539LDK705", {
+                page_title: document.title,
+                page_path: $page.url.pathname,
+            });
+        }
+    }
+</script>
+
+<svelte:head>
+    <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y539LDK705"
+    >
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag("js", new Date());
+        gtag("config", "G-Y539LDK705");
+    </script>
+</svelte:head>
