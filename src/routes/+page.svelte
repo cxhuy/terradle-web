34711a95972f85d63a43efbd1e2f7852027f5eb9
cxhuy <alvinkang1@gmail.com>
2023-10-11 11:16:58 -0500

    feat: add github link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -820,6 +820,7 @@
                 </p>
             </div>
         {/if}
+        <p class="mt-4 w-full text-center text-xl">Made by <a class="hover:underline" style="color: #FFFF0A" href="https://github.com/cxhuy" target="_blank">cxhuy</a></p>
     </main>
 {/if}
 
